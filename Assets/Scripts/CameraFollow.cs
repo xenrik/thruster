94d0a0a11923cd7ed2ceb1845797f4410ea55dc0
Lee Smith <lee@xenrik.com>
2019-04-30 21:29:10 +0100

    Initial Commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject Target;
+
+	public Vector3 Offset;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = Vector3.Lerp(transform.position, Target.transform.position + Offset, 0.9f);
+		transform.position = position;
+	}
+}
---
+++ Assets/Scripts/ThrustIt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrustIt : MonoBehaviour {
+
+	public float Force;
+
+	private Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 thrust = (Vector3.up * Force * Input.GetAxis("Vertical")) +
+			(-Vector3.forward * Force * Input.GetAxis("Horizontal"));
+		rigid.AddForce(thrust);
+	}
+}
