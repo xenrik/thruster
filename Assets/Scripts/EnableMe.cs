5c0cafc021b8eb9a70a75d9e056ffd36af818b12
Lee Smith <lee@xenrik.com>
2019-05-20 08:36:39 +0100

    Added Landing camera, Added Turret
    Added a landing camera that will appear when you are near the landing zone to aid dropping the cube
    Added a turret that follows you as you approach the cube (doesn't shoot yet)

---
+++ Assets/Scripts/EnableMe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableMe : MonoBehaviour {
+
+	public GameObject Target;
+	
+	private int triggerDepth;
+
+	private void OnTriggerEnter(Collider other) {
+		Target.SetActive(true);
+		++triggerDepth;
+	}
+
+	private void OnTriggerExit(Collider other) {
+		--triggerDepth;
+		if (triggerDepth == 0) {
+			Target.SetActive(false);	
+		}
+	}
+}
---
+++ Assets/Scripts/TurretFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretFollow : MonoBehaviour {
+
+	public GameObject Target;
+
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(Target.transform);
+	}
+}
