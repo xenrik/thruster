8d04e38864f88663ff698297db5d69d339ca4256
Lee Smith <lee@xenrik.com>
2019-05-29 09:07:24 +0100

    Working on custom slicer
    Added bulk of the code to support slicing (i think). Now "just" needs testing.

--- Assets/Scenes/TestSlicing/Scripts/SliceBehaviour.cs
+++ Assets/Scenes/TestSlicing/Scripts/SliceBehaviour.cs
@@ -1,10 +1,97 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SliceBehaviour : MonoBehaviour {
      private Slicer slicer;
 
+	public float Tolerance = 0.01f;
+	public float RotationTolerance = 1;
+
+	private Vector3 oldPosition;
+	private Quaternion oldRotation;
+
+	private Plane plane;
+
+	private HashSet<GameObject> slices = new HashSet<GameObject>();
+
      private void Start() {
           MeshFilter f = GetComponent<MeshFilter>();
           slicer = new Slicer(f.mesh);
+
+		oldPosition = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
+		oldRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawRay(transform.position, transform.up * 5, Color.red);
+		Vector3 offset = transform.position - oldPosition;
+		float rotationOffset = Quaternion.Angle(transform.rotation, oldRotation);
+		if (offset.magnitude > Tolerance || rotationOffset > RotationTolerance) {
+			foreach (GameObject slice in slices) {
+				Destroy(slice);
+			}
+			slices.Clear();
+
+			plane.SetNormalAndPosition(transform.up, transform.position);
+			Bounds b = new Bounds();
+			foreach (Renderer r in GetComponentsInChildren<Renderer>()) {
+				b.Encapsulate(r.bounds);
+			}
+
+			if (PlaneIntersects(plane, b)) {
+				Debug.Log("Slice!");
+
+                    slicer.slice(plane);
+                    
+                    GameObject posGO = new GameObject();
+                    MeshFilter posFilter = posGO.AddComponent<MeshFilter>();
+                    posFilter.mesh = slicer.posMesh;
+                    posGO.AddComponent<MeshRenderer>();
+                    
+                    GameObject negGO = new GameObject();
+                    MeshFilter negFilter = negGO.AddComponent<MeshFilter>();
+                    negFilter.mesh = slicer.negMesh;
+                    negGO.AddComponent<MeshRenderer>();
+
+                    slices.Add(posGO);
+                    slices.Add(negGO);
+				GetComponent<MeshRenderer>().enabled = false;
+			} else {
+				Debug.Log("OOB");
+				GetComponent<MeshRenderer>().enabled = true;
+			}
+
+			oldPosition = transform.position;
+			oldRotation = transform.rotation;
+		}
      }
+
+	private bool PlaneIntersects(Plane p, Bounds b) {
+		bool side = p.GetSide(b.min);
+
+		if (side != p.GetSide(b.max)) {
+			return true;
+		}
+		if (side != p.GetSide(new Vector3(b.min.x, b.min.y, b.max.z))) {
+			return true;
+		}
+		if (side != p.GetSide(new Vector3(b.min.x, b.max.y, b.min.z))) {
+			return true;
+		}
+		if (side != p.GetSide(new Vector3(b.min.x, b.max.y, b.max.z))) {
+			return true;
+		}
+		if (side != p.GetSide(new Vector3(b.max.x, b.min.y, b.min.z))) {
+			return true;
+		}
+		if (side != p.GetSide(new Vector3(b.max.x, b.min.y, b.max.z))) {
+			return true;
+		}
+		if (side != p.GetSide(new Vector3(b.max.x, b.max.y, b.min.z))) {
+			return true;
+		}
+
+		return false;
+	}
 }
--- Assets/Scenes/TestSlicing/Scripts/Slicer.cs
+++ Assets/Scenes/TestSlicing/Scripts/Slicer.cs
@@ -3,44 +3,159 @@ using UnityEngine;
 
 public class Slicer {
     private Mesh mesh;
+    private bool optimise;
 
     private object slicePos;
     private object sliceNeg;
 
-    public Slicer(Mesh mesh) {
+    private Ray ray;
+
+    public Mesh posMesh;
+    public Mesh negMesh;
+
+    public Slicer(Mesh mesh, bool optimise = false) {
         this.mesh = mesh;
+        this.optimise = optimise;
     }
 
     public void slice(Plane p) {
         Vector3[] vertices = mesh.vertices;
         int[] triangles = mesh.triangles;
         
-        Vector3?[] posVertices = new Vector3?[vertices.Length];
-        int?[] posTriangles = new int?[triangles.Length];
-
-        Vector3?[] negVertices = new Vector3?[vertices.Length];
-        int?[] negTriangles = new int?[triangles.Length];
+        List<Vector3> newVertices = new List<Vector3>();
+        List<int> posTriangles = new List<int>();
+        List<int> negTriangles = new List<int>();
+        Triangle t = new Triangle();
         
         for (int tri = 0; tri < triangles.Length; tri += 3) {
-            bool side1 = p.GetSide(vertices[tri]);
-            bool side2 = p.GetSide(vertices[tri+1]);
-            bool side3 = p.GetSide(vertices[tri+2]);
+            t.initialise(vertices, triangles[tri], triangles[tri + 1], triangles[tri + 2], p);
 
             // If all the points of the triangle are on the same side,
             // we don't need to split it.
-            if (side1 == side2 == side3) {
-                if (side1) {
-                    posVertices[tri] = vertices[tri];
-                    posVertices[tri+1] = vertices[tri+1];
-                    posVertices[tri+2] = vertices[tri+2];
+            if (t.aSide == t.bSide == t.cSide) {
+                if (t.aSide) {
+                    posTriangles.AddRange(new int[] { tri, tri+1, tri+2 });
                 } else {
-                    negVertices[tri] = vertices[tri];
-                    negVertices[tri+1] = vertices[tri+1];
-                    negVertices[tri+2] = vertices[tri+2];
+                    negTriangles.AddRange(new int[] { tri, tri+1, tri+2 });
                 }
             } else {
                 // Split the triangle
+                splitTriangle(t, p, newVertices, posTriangles, negTriangles, vertices.Length);
             }
+        }
+
+        // TODO - Optimise
+        // if (optimise) {
+        //    ...
+        // }
+
+        Vector3[] allVertices = new Vector3[vertices.Length + newVertices.Count];
+        System.Array.Copy(vertices, allVertices, vertices.Length);
+        newVertices.CopyTo(0, allVertices, vertices.Length, newVertices.Count);
+
+        Mesh posMesh = new Mesh();
+        posMesh.vertices = allVertices;
+        posMesh.triangles = posTriangles.ToArray();
+        posMesh.RecalculateBounds();
+        posMesh.RecalculateNormals();
+
+        Mesh negMesh = new Mesh();
+        negMesh.vertices = allVertices;
+        negMesh.triangles = negTriangles.ToArray();
+        negMesh.RecalculateBounds();
+        negMesh.RecalculateNormals();
+    }
+
+    private void splitTriangle(Triangle t, Plane p, List<Vector3> newVertices, List<int> posTriangles, List<int> negTriangles, int vertexOffset) {
+        // Find which edges need splitting
+        bool splitAB = t.aSide != t.bSide;
+        bool splitBC = t.bSide != t.cSide;
+        bool splitCA = t.cSide != t.aSide;
+
+        int cut1i = vertexOffset + newVertices.Count;
+        int cut2i = cut1i + 1;
+
+        if (splitAB && splitBC) {
+            newVertices.Add(getCut(t.a, t.b, p)); // cut1
+            newVertices.Add(getCut(t.b, t.c, p)); // cut2
+
+            if (t.aSide) {
+                posTriangles.AddRange(new int[] { t.ai, cut1i, cut2i });
+                negTriangles.AddRange(new int[] { cut1i, t.bi, cut2i });
+                posTriangles.AddRange(new int[] { cut2i, t.ci, t.ai });
+            } else {
+                negTriangles.AddRange(new int[] { t.ai, cut1i, cut2i });
+                posTriangles.AddRange(new int[] { cut1i, t.bi, cut2i });
+                negTriangles.AddRange(new int[] { cut2i, t.ci, t.ai });
+            }
+        } else if (splitAB && splitCA) {
+            newVertices.Add(getCut(t.a, t.b, p)); // cut1
+            newVertices.Add(getCut(t.c, t.a, p)); // cut2
+
+            if (t.aSide) {
+                posTriangles.AddRange(new int[] { t.ai, cut1i, cut2i });
+                negTriangles.AddRange(new int[] { cut1i, t.bi, cut2i });
+                negTriangles.AddRange(new int[] { t.bi, t.ci, cut2i });
+            } else {
+                negTriangles.AddRange(new int[] { t.ai, cut1i, cut2i });
+                posTriangles.AddRange(new int[] { cut1i, t.bi, cut2i });
+                posTriangles.AddRange(new int[] { t.bi, t.ci, cut2i });
+            }
+        } else {
+            // Must be splitBC && splitCA
+            newVertices.Add(getCut(t.b, t.c, p)); // cut1
+            newVertices.Add(getCut(t.c, t.a, p)); // cut2
+
+            if (t.aSide) {
+                posTriangles.AddRange(new int[] { t.ai, cut1i, cut2i });
+                posTriangles.AddRange(new int[] { t.ai, t.bi, cut1i });
+                negTriangles.AddRange(new int[] { cut1i, t.ci, cut2i });
+            } else {
+                negTriangles.AddRange(new int[] { t.ai, cut1i, cut2i });
+                negTriangles.AddRange(new int[] { t.ai, t.bi, cut1i });
+                posTriangles.AddRange(new int[] { cut1i, t.ci, cut2i });
+            }
+        }
+    }
+
+    private Vector3 getCut(Vector3 a, Vector3 b, Plane p) {
+        ray.origin = a;
+        ray.direction = b - a;
+        float length;
+        if (!p.Raycast(ray, out length)) {
+            // Something went wrong!
+            Debug.Log("Unexpectedly didn't intersect the plane?");
+            return Vector3.zero;
+        }
+
+        return ray.GetPoint(length);
+    }
+
+    private struct Triangle {
+        public int ai;
+        public Vector3 a;
+        public bool aSide;
+
+        public int bi;
+        public Vector3 b;
+        public bool bSide;
+
+        public int ci;
+        public Vector3 c;
+        public bool cSide;
+
+        public void initialise(Vector3[] vertices, int ai, int bi, int ci, Plane p) {
+            this.ai = ai;
+            a = vertices[ai];
+            aSide = p.GetSide(a);
+
+            this.bi = bi;
+            b = vertices[bi];
+            bSide = p.GetSide(b);
+
+            this.ci = ci;
+            c = vertices[ci];
+            cSide = p.GetSide(c);
         }
     }
 }
